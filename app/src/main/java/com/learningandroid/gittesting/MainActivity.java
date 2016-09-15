1fd0bf806ad4a5efcd29b13574e4d099a8e85923
masterlopez <javy7@hotmail.com>
2016-09-15 12:08:44 -0400

    Changed 2 things

--- app/src/main/java/com/learningandroid/gittesting/MainActivity.java
+++ app/src/main/java/com/learningandroid/gittesting/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "It is I", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "It is I, Your Master", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
